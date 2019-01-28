a3b30fd4d41e470bfde7aa719dcb0aabccfc9ab6
Almog Gabay <theninesky@gmail.com>
2019-01-28 20:57:18 +0000

    first git backup

---
+++ app.js
@@ -0,0 +1,10 @@
+let express = require("express"),
+    app     = express();
+
+app.use(express.static(__dirname + '/public'));
+    
+app.get("/", function(req, res) {
+    res.sendfile("checkers.html");
+});
+
+app.listen(process.env.PORT, process.env.IP);
---
+++ backup/checkers.js
@@ -0,0 +1,165 @@
+console.log("Width: " + window.innerWidth + ", Height: " + window.innerHeight); // Width: 1920, Height: 938
+/* Define the entire tiles array which includes all of the black tiles on the board */
+let tiles = [
+        undefined,
+        document.querySelectorAll(".first"),
+        document.querySelectorAll(".second"),
+        document.querySelectorAll(".third"),
+        document.querySelectorAll(".fourth"),
+        document.querySelectorAll(".fifth"),
+        document.querySelectorAll(".sixth"),
+        document.querySelectorAll(".seventh"),
+        document.querySelectorAll(".eighth")
+    ];
+let turn = "black-pawn", blackPawns = 12, whitePawns = 12, selectedPawn;
+
+let captureOccured = {
+    value: false,
+    row: undefined,
+    rowStep: undefined,
+    tile: undefined,
+    tileStep: undefined,
+    execute: function(row = this.row, rowStep = this.rowStep, tile = this.tile, tileStep = this.tileStep, captureType) {
+        switch (captureType) {
+            case 1: {
+                break;
+            }
+            case 2: {
+                break;
+            }
+            case 3: {
+                break;
+            }
+            case 4: {
+                break;
+            }
+        }
+    }
+}
+
+let printTurn = document.querySelector("#turn"),
+    startButton = document.querySelector("#start");
+
+let setTiles = {
+    black: tile => tile.classList.add("black-pawn"),
+    white: tile => tile.classList.add("white-pawn")
+};
+
+startButton.addEventListener("click", () => {
+    gameStart();
+    startButton.style.opacity = "0";
+});
+startButton.addEventListener("transitionend", function() {this.style.display = "none"});
+
+/* Places all the pawns in their initial position */
+function gameStart() { 
+    tiles[1].forEach(setTiles.black);
+    tiles[2].forEach(setTiles.black);
+    tiles[3].forEach(setTiles.black);
+    tiles[6].forEach(setTiles.white);
+    tiles[7].forEach(setTiles.white);
+    tiles[8].forEach(setTiles.white);
+    printTurn.textContent = turn;
+    for(let i = 1; i < tiles.length; i++) {
+        for(let j = 0; j < 4; j++) {
+            tiles[i][j].addEventListener("click", () => checkMovement(i, j));
+        }
+    }
+}
+
+function checkMovement(row, tile) {
+    if (tiles[row][tile].classList.contains("suggested-move")) { // if a suggested path is chosen
+        if (captureOccured) {
+            captureOccured();
+        }
+        else {
+            selectedPawn.classList.remove(turn);
+            tiles[row][tile].classList.add(turn); 
+        }
+        turn = (turn == "black-pawn") ?  "white-pawn" : "black-pawn"; // switch turns
+        printTurn.textContent = turn;
+    }
+    clearSuggestions();
+    if (tiles[row][tile].classList.contains(turn)) { 
+        if (turn == "black-pawn") {
+            (row%2 != 0) ? showSuggestions(row, tile, 1, -1) : showSuggestions(row, tile, 1, 1);
+        }
+        else { // white turn
+            (row%2 != 0) ? showSuggestions(row, tile, -1, -1) : showSuggestions(row, tile, -1, 1);
+        }
+    selectedPawn = tiles[row][tile];
+    }
+}
+
+function clearSuggestions() {
+    for (let i = 1; i < tiles.length; i++) {
+        for(let j = 0; j < 4; j++) {
+            tiles[i][j].classList.remove("suggested-move");
+        }
+    }
+}
+
+function oldShowSuggestions(row, tile, rowStep, tileStep) {
+    if(tiles[row+rowStep][tile+tileStep] && !tiles[row+rowStep][tile+tileStep].classList.contains("black-pawn") && !tiles[row+rowStep][tile+tileStep].classList.contains("white-pawn")) { // makes sure that suggested area isn't taken
+        tiles[row+rowStep][tile+tileStep].classList.add("suggested-move");
+    }
+    if(tiles[row+rowStep][tile] && !tiles[row+rowStep][tile].classList.contains("black-pawn") && !tiles[row+rowStep][tile].classList.contains("white-pawn")) { // makes sure that suggested area isn't taken
+        tiles[row+rowStep][tile].classList.add("suggested-move");
+    }
+}
+
+function showSuggestions(row, tile, rowStep, tileStep) { // currently designed only for black turn
+    if (turn == "black-pawn") { // black turn
+        if (tiles[row+rowStep][tile+tileStep] && !tiles[row+rowStep][tile+tileStep].classList.contains("black-pawn")) {
+            if (tiles[row+rowStep][tile+tileStep].classList.contains("white-pawn")) {
+                if (tiles[row+rowStep*2][tile+tileStep] && !tiles[row+rowStep*2][tile+tileStep].classList.contains("black-pawn") && !tiles[row+rowStep*2][tile+tileStep].classList.contains("white-pawn")) {
+                    captureOccured = function(row, rowStep, tile, tileStep) {
+                    tiles[row+rowStep*2][tile+tileStep].classList.add("suggested-move");
+                    tiles[row+rowStep][tile+tileStep].classList.remove("white-pawn");
+                    whitePawns--;
+                }  
+            }
+            else {
+                tiles[row+rowStep][tile+tileStep].classList.add("suggested-move");
+            }
+        }
+        if (tiles[row+rowStep][tile] && !tiles[row+rowStep][tile].classList.contains("black-pawn")) {
+            if (tiles[row+rowStep][tile].classList.contains("white-pawn")) {
+                if (tiles[row+rowStep*2][tile+tileStep*(-1)] && !tiles[row+rowStep*2][tile+tileStep*(-1)].classList.contains("black-pawn") && !tiles[row+rowStep*2][tile+tileStep*(-1)].classList.contains("white-pawn")) {
+                    tiles[row+rowStep*2][tile+tileStep*(-1)].classList.add("suggested-move");
+                    tiles[row+rowStep][tile].classList.remove("white-pawn");
+                    whitePawns--;
+                } 
+            }
+            else {
+                tiles[row+rowStep][tile].classList.add("suggested-move");
+            }
+        }   
+    }
+    else { // white turn
+        if (tiles[row+rowStep][tile+tileStep] && !tiles[row+rowStep][tile+tileStep].classList.contains("white-pawn")) {
+            if (tiles[row+rowStep][tile+tileStep].classList.contains("black-pawn")) {
+                if (tiles[row+rowStep*2][tile+tileStep] && !tiles[row+rowStep*2][tile+tileStep].classList.contains("black-pawn") && !tiles[row+rowStep*2][tile+tileStep].classList.contains("white-pawn")) {
+                    tiles[row+rowStep*2][tile+tileStep].classList.add("suggested-move");
+                    tiles[row+rowStep][tile+tileStep].classList.remove("black-pawn");
+                    blackPawns--;
+                }  
+            }
+            else {
+                tiles[row+rowStep][tile+tileStep].classList.add("suggested-move");
+            }
+        }
+        if (tiles[row+rowStep][tile] && !tiles[row+rowStep][tile].classList.contains("white-pawn")) {
+            if (tiles[row+rowStep][tile].classList.contains("black-pawn")) {
+                if (tiles[row+rowStep*2][tile+tileStep*(-1)] && !tiles[row+rowStep*2][tile+tileStep*(-1)].classList.contains("black-pawn") && !tiles[row+rowStep*2][tile+tileStep*(-1)].classList.contains("white-pawn")) {
+                    tiles[row+rowStep*2][tile+tileStep*(-1)].classList.add("suggested-move");
+                    tiles[row+rowStep][tile].classList.remove("black-pawn");
+                    blackPawns--;
+                } 
+            }
+            else {
+                tiles[row+rowStep][tile].classList.add("suggested-move");
+            }
+        }
+    }
+}
